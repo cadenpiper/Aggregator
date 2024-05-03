32879a1208fe6c1d2838831667ae026c5f0b35f2
cadenpiper <120049430+cadenpiper@users.noreply.github.com>
2024-05-03 17:00:46 -0500

    Adding Liquidity

--- contracts/Aggregator.sol
+++ contracts/Aggregator.sol
@@ -12,6 +12,14 @@ contract Aggregator {
 	AMM public amm1;
 	AMM public amm2;
 
+	uint256 public token1Balance;
+	uint256 public token2Balance;
+	uint256 public K;
+
+	uint256 public totalShares;
+    mapping(address => uint256) public shares;
+    uint256 constant PRECISION = 10**18;
+
 	constructor(
 		string memory _name,
 		Token _token1,
@@ -26,35 +34,106 @@ contract Aggregator {
 		amm2 = _amm2;
 	}
 
-	// Fetch best prices from AMMs
-	function token2SwapBestPrice(uint256 _inputAmount)
-		public
-		view
-		returns (uint256, address)
-	{
-		uint256 outputAmountAmm1 = amm1.calculateToken2Swap(_inputAmount);
-		uint256 outputAmountAmm2 = amm2.calculateToken2Swap(_inputAmount);
+	function addLiquidity(uint256 _token1Amount, uint256 _token2Amount) external {
+		// Add liquidity
+		require(token1.transferFrom(msg.sender, address(this), _token1Amount));
+		require(token2.transferFrom(msg.sender, address(this), _token2Amount));
 
-		if (outputAmountAmm1 > outputAmountAmm2) {
-			return (outputAmountAmm1, address(amm1));
-		} else {
-			return (outputAmountAmm2, address(amm2));
-		} 
+		// Issue shares
+		// If first time adding liquidity, all shares go to initial liquidity provider
+		uint256 share;
+		if (totalShares == 0) {
+            share = 100 * PRECISION;
+        } else {
+            uint256 share1 = (totalShares * _token1Amount) / token1Balance;
+            uint256 share2 = (totalShares * _token2Amount) / token2Balance;
+            require(
+                (share1 / 10**3) == (share2 / 10**3),
+                "must provide equal token amounts"
+            );
+            share = share1;
+        }
+
+        // Manage liquidity
+		token1Balance += _token1Amount;
+		token2Balance += _token2Amount;
+		K = token1Balance * token2Balance;
+
+		// Update shares
+        totalShares += share;
+        shares[msg.sender] += share;
 	}
 
-	function token1SwapBestPrice(uint256 _inputAmount)
+	// Determine amount of token1 to deposit by token2 input amount
+	function calculateToken1Deposit(uint256 _token2Amount)
 		public
 		view
-		returns (uint256, address)
+		returns(uint256 token1Amount)
 	{
-		uint256 outputAmountAmm1 = amm1.calculateToken1Swap(_inputAmount);
-		uint256 outputAmountAmm2 = amm2.calculateToken1Swap(_inputAmount);
+		token1Amount = (token1Balance - _token2Amount) / token2Balance;
+	}
 
-		if (outputAmountAmm1 > outputAmountAmm2) {
-			return (outputAmountAmm1, address(amm1));
+	// Determine amount of token2 to deposit by token1 input amount
+	function calculateToken2Deposit(uint256 _token1Amount)
+		public
+		view
+		returns(uint256 token2Amount)
+	{
+		token2Amount = (token2Balance - _token1Amount) / token1Balance;
+	}
+
+	function getBestToken1Price(uint256 _amount) public view returns (uint256, address) {
+		uint256 token1OutputAmm1 = amm1.calculateToken2Swap(_amount);
+		uint256 token1OutputAmm2 = amm2.calculateToken2Swap(_amount);
+
+		if (token1OutputAmm1 > token1OutputAmm2) {
+			return(token1OutputAmm2, address(amm2));
 		} else {
-			return (outputAmountAmm2, address(amm2));
+			return(token1OutputAmm1, address(amm1));
 		}
 	}
 
+	function getBestToken2Price(uint256 _amount) public view returns (uint256, address) {
+		uint256 token2OutputAmm1 = amm1.calculateToken2Swap(_amount);
+		uint256 token2OutputAmm2 = amm2.calculateToken2Swap(_amount);
+
+		if(token2OutputAmm1 > token2OutputAmm2) {
+			return(token2OutputAmm2, address(amm2));
+		} else {
+			return(token2OutputAmm1, address(amm1));
+		}
+	}
+
+	function executeSwap(uint256 _amount) external returns (uint256, address) {
+		(uint256 bestToken1Output, address bestAmm) = getBestToken1Price(_amount);
+
+		if (bestAmm == address(amm1)) {
+			amm1.swapToken2(_amount);
+			return(bestToken1Output, address(amm1));
+		} else {
+			amm2.swapToken2(_amount);
+			return(bestToken1Output, address(amm2));
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test/Aggregator.js
+++ test/Aggregator.js
@@ -9,82 +9,54 @@ const ether = tokens
 const shares = ether
 
 describe('Aggregator', () => {
-	let token1,
-			token2,
-			amm1,
-			amm2
-
 	let accounts,
 			deployer,
 			liquidityProvider
 
+	let token1,
+			token2,
+			amm1,
+			amm2,
+			aggregator
+
 	beforeEach(async () => {
-		// Setup accounts
+		// Set up accounts
 		accounts = await ethers.getSigners()
 		deployer = accounts[0]
 		liquidityProvider = accounts[1]
 
-		// Deploy contracts
+		// Deploy token contracts
 		const Token = await ethers.getContractFactory('Token')
-		const AMM = await ethers.getContractFactory('AMM')
-		const Aggregator = await ethers.getContractFactory('Aggregator')
-
-		token1 = await Token.deploy('Honey Token', 'HNY', '1000000')
+		token1 = await Token.deploy('token1', 'TKN1', '1000000')
 		await token1.deployed()
-		token2 = await Token.deploy('USD Token', 'USD', '1000000')
+		token2 = await Token.deploy('token2', 'TKN2', '1000000')
 		await token2.deployed()
 
-		amm1 = await AMM.deploy('Sunflower AMM', token1.address, token2.address)
-		await amm1.deployed()
-		amm2 = await AMM.deploy('Clover AMM', token1.address, token2.address)
-		await amm2.deployed()
+    // Deploy amm contracts
+    const AMM = await ethers.getContractFactory('AMM')
+    amm1 = await AMM.deploy('amm1', token1.address, token2.address)
+    await amm1.deployed()
+    amm2 = await AMM.deploy('amm2', token1.address, token2.address)
+    await amm2.deployed()
 
-		// Send tokens to liquidity provider
-		let transaction = await token1.connect(deployer).transfer(liquidityProvider.address, tokens(200000))
-		await transaction.wait()
-		transaction = await token2.connect(deployer).transfer(liquidityProvider.address, tokens(200000))
-		await transaction.wait()
-
-		aggregator = await Aggregator.deploy(
-			'Hive Aggregator',
-			token1.address,
-			token2.address,
-			amm1.address,
-			amm2.address
-		)
-		await aggregator.deployed()
+    // Deploy aggregator
+    const Aggregator = await ethers.getContractFactory('Aggregator')
+    aggregator = await Aggregator.deploy(
+    	'aggregator',
+    	token1.address,
+    	token2.address,
+    	amm1.address,
+    	amm2.address
+    )
+    await aggregator.deployed()
 	})
 
 	describe('Deployment', () => {
-		const decimals = '18'
-		const totalSupply = tokens(1000000)
-		
-		it('has correct names', async () => {
-			expect(await token1.name()).to.equal('Honey Token')
-			expect(await token2.name()).to.equal('USD Token')
-			expect(await amm1.name()).to.equal('Sunflower AMM')
-			expect(await amm2.name()).to.equal('Clover AMM')
-			expect(await aggregator.name()).to.equal('Hive Aggregator')
-		})
-
-		it('has correct symbols', async () => {
-			expect(await token1.symbol()).to.equal('HNY')
-			expect(await token2.symbol()).to.equal('USD')
-		})
-
-		it('has correct decimals', async () => {
-			expect(await token1.decimals(), token2.decimals()).to.equal(decimals)
-		})
-
-		it('has correct totalSupply', async () => {
-			expect(await token1.totalSupply(), token2.totalSupply()).to.equal(totalSupply)
+		it('has correct name', async () => {
+			expect(await aggregator.name()).to.equal('aggregator')
 		})
 
 		it('tracks token addresses', async () => {
-			expect(await amm1.token1()).to.equal(token1.address)
-			expect(await amm1.token2()).to.equal(token2.address)
-			expect(await amm2.token1()).to.equal(token1.address)
-			expect(await amm2.token2()).to.equal(token2.address)
 			expect(await aggregator.token1()).to.equal(token1.address)
 			expect(await aggregator.token2()).to.equal(token2.address)
 		})
@@ -95,101 +67,83 @@ describe('Aggregator', () => {
 		})
 	})
 
-	describe('AMM Liquidity', () => {
-		let amount, transaction
+	describe('Liquidity', () => {
+		let transaction
 
 		beforeEach(async () => {
-			amount = tokens(100000)
-			amount1 = tokens(100)
-
-			// Liquidity provider approves tokens for amm1 & amm2
-			transaction = await token1.connect(liquidityProvider).approve(amm1.address, amount)
+			// Transfer tokens to liquidity provider to provide liquidity
+			transaction = await token1.connect(deployer).transfer(liquidityProvider.address, tokens(10000))
 			await transaction.wait()
-			transaction = await token2.connect(liquidityProvider).approve(amm1.address, amount)
-			await transaction.wait()
-			transaction = await token1.connect(liquidityProvider).approve(amm2.address, amount)
-			await transaction.wait()
-			transaction = await token2.connect(liquidityProvider).approve(amm2.address, amount)
+			transaction = await token2.connect(deployer).transfer(liquidityProvider.address, tokens(10000))
 			await transaction.wait()
 
-			// Liquidity provider adds liquidity for amm1 & amm2
-			transaction = await amm1.connect(liquidityProvider).addLiquidity(amount, amount)
+			// Approve tokens
+			transaction = await token1.connect(liquidityProvider).approve(aggregator.address, tokens(10000))
 			await transaction.wait()
-			transaction = await amm2.connect(liquidityProvider).addLiquidity(amount, amount)
+			transaction = await token2.connect(liquidityProvider).approve(aggregator.address, tokens(10000))
+			await transaction.wait()
+
+			// Add liquidity
+			transaction = await aggregator.connect(liquidityProvider).addLiquidity(tokens(500), tokens(500))
 			await transaction.wait()
 		})
 
-		it('approves and adds liquidity for both AMMs', async () => {
-			expect(await token1.balanceOf(amm1.address)).to.equal(amount)
-			expect(await token2.balanceOf(amm1.address)).to.equal(amount)
-			expect(await token1.balanceOf(amm2.address)).to.equal(amount)
-			expect(await token2.balanceOf(amm2.address)).to.equal(amount)
+		it('receives liquidity', async () => {
+			expect(await token1.balanceOf(liquidityProvider.address)).to.equal(tokens(9500))
+			expect(await token1.allowance(liquidityProvider.address, aggregator.address)).to.equal(tokens(9500))
+
+			expect(await aggregator.token1Balance()).to.equal(tokens(500))
+			expect(await aggregator.token2Balance()).to.equal(tokens(500))
 		})
 
-		it('tracks user shares', async () => {
-			expect(await amm1.shares(liquidityProvider.address)).to.equal(tokens(100))
-			expect(await amm2.shares(liquidityProvider.address)).to.equal(tokens(100))
-		})
-	})
-
-	describe('Fetching Best Swap Prices', () => {
-		let amount,
-				amount1
-
-		beforeEach(async () => {
-			amount = tokens(250)
-			amount1 = tokens(500)
-
-			// Liquidity provider approves tokens for amm1 & amm2
-			transaction = await token1.connect(liquidityProvider).approve(amm1.address, amount)
-			await transaction.wait()
-			transaction = await token2.connect(liquidityProvider).approve(amm1.address, amount1)
-			await transaction.wait()
-			transaction = await token1.connect(liquidityProvider).approve(amm2.address, amount1)
-			await transaction.wait()
-			transaction = await token2.connect(liquidityProvider).approve(amm2.address, amount)
-			await transaction.wait()
-
-			// Liquidity provider adds liquidity for amm1 & amm2
-			transaction = await amm1.connect(liquidityProvider).addLiquidity(amount, amount1)
-			await transaction.wait()
-			transaction = await amm2.connect(liquidityProvider).addLiquidity(amount1, amount)
-			await transaction.wait()
+		it('checks shares', async () => {
+			expect(await aggregator.shares(liquidityProvider.address)).to.equal(tokens(100))
+			expect(await aggregator.totalShares()).to.equal(tokens(100))
 		})
 
-		it('calculates best price and amm for token2 swap', async () => {
-			let [token2BestSwapPrice, bestAmm] = await aggregator.token2SwapBestPrice(amount)
-
-			// token2 liquidity for amm1 & amm2
-			expect(await amm1.token2Balance()).to.equal(tokens(500))
-			expect(await amm2.token2Balance()).to.equal(tokens(250))
-
-			console.log(`Best token2 swap price: ${ethers.utils.formatEther(token2BestSwapPrice)}`)
-			expect(await token2BestSwapPrice).to.equal(tokens(250))
-
-			if (await bestAmm === amm1.address) {
-				console.log(`Best token2 swap price at: ${await amm1.name()}`)
-			} else {
-				console.log(`Best token2 swap price at: ${await amm2.name()}`)
-			}
-		})
-
-		it('calculates best price and amm for token1 swap', async () => {
-			let [token1BestSwapPrice, bestAmm] = await aggregator.token1SwapBestPrice(amount)
-
-			// token1 liquidity for amm1 & amm2
-			expect(await amm1.token2Balance()).to.equal(tokens(500))
-			expect(await amm2.token2Balance()).to.equal(tokens(250))
-
-			console.log(`Best token1 swap price: ${ethers.utils.formatEther(token1BestSwapPrice)}`)
-			expect(await token1BestSwapPrice).to.equal(tokens(250))
-
-			if (await bestAmm === amm1.address) {
-				console.log(`Best token1 swap price at: ${await amm1.name()}`)
-			} else {
-				console.log(`Best token1 swap price at: ${await amm1.name()}`)
-			}
+		it('calculates deposits', async () => {
+			expect(await aggregator.calculateToken1Deposit(tokens(500)))
+			expect(await aggregator.calculateToken2Deposit(tokens(500)))
 		})
 	})
-
 })
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
